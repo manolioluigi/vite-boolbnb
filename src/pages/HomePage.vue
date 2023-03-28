65ff2c10ebf021ea6d9eea340f940438a658b659
manolioluigi <luigi.manolio1999@gmail.com>
2023-03-28 16:37:05 +0200

    push iniziale

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        name: 'HomePage',
+    }
+</script>
+
+<template>
+    <h1>BoolBnB</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from './pages/HomePage.vue';
+// import NotFound from './pages/NotFound.vue';
+// import ThankYou from './pages/ThankYou.vue';
+// import Contacts from './pages/Contacts.vue';
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'HomePage',
+            component: HomePage,
+        },
+        // {
+        //     path: '/Contatti',
+        //     name: 'Contacts',
+        //     component: Contacts
+        // },
+        // {
+        //     path: '/Thank-You',
+        //     name: 'ThankYou',
+        //     component: ThankYou
+        // },
+        // {
+        //     path: '/*',
+        //     name: 'NotFound',
+        //     component: NotFound
+        // },
+
+    ]
+});
+
+export { router };
