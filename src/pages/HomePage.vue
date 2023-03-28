7964b1edff9cdc6e13b8ba68f2f3b79c553bcbde
manolioluigi <luigi.manolio1999@gmail.com>
2023-03-28 17:06:06 +0200

    homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -48,41 +48,33 @@
                 </div>
                 <div v-else class="col-12 d-flex justify-content-center flex-wrap">
                     <div class="row">
-                        <div class="col-12 col-md-6 col-lg-4" v-for="project in projects" :key="project.id">
+                        <div class="col-12 col-md-6 col-lg-4" v-for="apartment in apartments" :key="apartment.id">
                             
 
                             <div class="card my-3">
                                 <div class="card-body">
                                     <div class="card-img-top d-flex justify-content-center">
-                                        <img :src="project.cover_image != null ? `${store.baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/300/200'" :alt="project.title" class="img-fluid">
+                                        <img :src="apartment.cover_img != null ? `${store.baseUrl}/storage/${apartment.cover_img}` : 'https://picsum.photos/300/200'" :alt="apartment.title" class="img-fluid">
                                     </div>
                                     <div class="card-title py-1 d-flex flex-column">
-                                        <h5>{{ project.title }}</h5>
+                                        <h5>{{ apartment.title }}</h5>
+                                        
                                         <em> 
-                                            <strong>Tipo: </strong> 
-                                            <span  v-if="project.type">
-                                                {{ project.type.name }}
+                                            <strong>Optionals: </strong> 
+                                            <span v-if="apartment.optionals.length > 0">
+                                                <span class="badge bg-primary mx-1" v-for="optional in apartment.optionals" :key="optional.id">{{ optional.title }}</span>
                                             </span>
                                             <span v-else>
-                                                Nessun tipo
-                                            </span>
-                                        </em>
-                                        <em> 
-                                            <strong>Tecnologie: </strong> 
-                                            <span v-if="project.technologies.length > 0">
-                                                <span class="badge bg-primary mx-1" v-for="item in project.technologies">{{ item.name }}</span>
-                                            </span>
-                                            <span v-else>
-                                                Nessuna tecnologia
+                                                No optionals
                                             </span>
                                         </em>
                                     </div>
                                     <div class="card-text py-1">
-                                        {{ project.content }}
+                                        {{ apartment.description }}
                                     </div>
-                                    <router-link :to="{ name: 'SingleProject', params: { slug: project.slug } }" class="btn btn-sm btn-success my-3">
+                                    <!-- <router-link :to="{ name: 'SingleProject', params: { slug: apartment.slug } }" class="btn btn-sm btn-success my-3">
                                         Continua a leggere
-                                    </router-link>
+                                    </router-link> -->
                                 </div>
                             </div>
 
@@ -92,17 +84,17 @@
                     <div class="row">
                         <div class="col-12">
                             <nav>
-                                <!-- <ul class="pagination">
+                                <ul class="pagination">
                                     <li :class="currentPage == 1 ? 'disabled' : 'page-item'">
-                                        <button class="page-link" @click="getProjects(currentPage - 1)">Prev</button>
+                                        <button class="page-link" @click="getApartments(currentPage - 1)">Prev</button>
                                     </li>
-                                    <li :class="currentPage == i ? 'disabled' : 'page-item'" v-for="i in lastPage">
-                                        <button class="page-link" @click="getProjects(i)">{{ i }}</button>
+                                    <li :class="currentPage == i ? 'disabled' : 'page-item'" v-for="i in lastPage" :key="i">
+                                        <button class="page-link" @click="getApartments(i)">{{ i }}</button>
                                     </li>
                                     <li :class="currentPage == lastPage ? 'disabled' : 'page-item'">
-                                        <button class="page-link" @click="getProjects(currentPage + 1)">Next</button>
+                                        <button class="page-link" @click="getApartments(currentPage + 1)">Next</button>
                                     </li>
-                                </ul> -->
+                                </ul>
                             </nav>
                         </div>
                     </div>
