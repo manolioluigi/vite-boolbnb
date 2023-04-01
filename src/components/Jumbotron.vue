f1eb9d95c7837e399e6377939f7e2089144d6e3b
manolioluigi <luigi.manolio1999@gmail.com>
2023-04-01 13:01:54 +0200

    final push

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -15,7 +15,7 @@ export default {
         addresses: [],
         filterData: {
             address: '',
-            distance: 20,
+            distance: 20000,
             bed_n: 0,
             room_n: 0,
             optionals: [],
@@ -108,9 +108,8 @@ export default {
                                         </ul>
                                     </div>
                                 </div>
-
     
-                                <div>
+                                <!-- <div>
                                     <h4 class="mb-3">Search your appartament</h4>
                                     <div class="container-input mb-3">
                                         <input type="text" placeholder="Roma, Viale del Popolo, 0000" name="text" class="input" id="searchbar" @keyup="autoComplete">
@@ -118,7 +117,7 @@ export default {
                                             <path d="M790.588 1468.235c-373.722 0-677.647-303.924-677.647-677.647 0-373.722 303.925-677.647 677.647-677.647 373.723 0 677.647 303.925 677.647 677.647 0 373.723-303.924 677.647-677.647 677.647Zm596.781-160.715c120.396-138.692 193.807-319.285 193.807-516.932C1581.176 354.748 1226.428 0 790.588 0S0 354.748 0 790.588s354.748 790.588 790.588 790.588c197.647 0 378.24-73.411 516.932-193.807l516.028 516.142 79.963-79.963-516.142-516.028Z" fill-rule="evenodd"></path>
                                         </svg>
                                     </div>
-                                </div> -->
+                                </div>  -->
                                 <div>
                                     <div class="d-flex align-items-center mb-3">
                                         <i class="fas fa-sliders"></i>
