7b1b66cc7b4e617382d9279693e089f7fb45533d
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-04-06 18:11:03 +0200

    chiusura btn offcanvas

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -196,7 +196,7 @@ export default {
                                     </div>
                                 </div>
                                 <div class="my-3">
-                                    <button class="cssbuttons-io-button" type="button" @click="this.submitData"> Search
+                                    <button class="cssbuttons-io-button" type="button" @click="this.submitData" data-bs-dismiss="offcanvas" aria-label="Close"> Search
                                         <div class="icon">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
                                         </div>
