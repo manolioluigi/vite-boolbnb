f57bfc42004783c2a24b3ee3676f287d8ff0be2d
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-04-06 17:27:02 +0200

    push filippo

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -44,41 +44,40 @@
 </script>
 
 <template>
-
-    <div class="bg-pink">
-
+    <div>
         <Jumbotron :jumbotron="jumbotron"></Jumbotron>
-        
-        <div class="container">
-            <div class="row">
-                <div v-if="loading" class="col-12 d-flex justify-content-center">
-                    <div class="my-5 loader"></div> 
+    </div>
+    <div class="container">
+        <div class="row">
+            <div v-if="loading" class="col-12 d-flex justify-content-center">
+                <div class="my-5 loader"></div> 
+            </div>
+            <div v-else class="col-12 d-flex justify-content-center flex-wrap">
+                <div class="row filtered-data-container">
+                    
+                    <div class="col-12 col-md-6 col-lg-4 my-5" v-for="apartment in apartments" :key="apartment.id">
+                        <ApartmentCard :apartment="apartment" v-if="!store.filterFlag"></ApartmentCard>
+                    </div>
+                    
+                    <FilteredApartmentCard v-if="store.filterFlag"></FilteredApartmentCard>
+                    
                 </div>
-                <div v-else class="col-12 d-flex justify-content-center flex-wrap">
-                    <div class="row filtered-data-container">
-                        <div class="d-flex" v-if="!store.filterFlag">
-                            <div class="col-12 col-md-6 col-lg-4 my-5 px-3" v-for="apartment in apartments" :key="apartment.id">
-                                <ApartmentCard :apartment="apartment"></ApartmentCard>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 d-flex justify-content-center my-5">
-                            <nav>
-                                <ul class="pagination">
-                                    <li :class="currentPage == 1 ? 'disabled' : 'page-item'">
-                                        <button class="page-link" @click="getApartments(currentPage - 1)">Prev</button>
-                                    </li>
-                                    <li :class="currentPage == i ? 'disabled' : 'page-item'" v-for="i in lastPage" :key="i">
-                                        <button class="page-link" @click="getApartments(i)">{{ i }}</button>
-                                    </li>
-                                    <li :class="currentPage == lastPage ? 'disabled' : 'page-item'">
-                                        <button class="page-link" @click="getApartments(currentPage + 1)">Next</button>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
+            </div>
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center my-5">
+                    <nav>
+                        <ul class="pagination">
+                            <li :class="currentPage == 1 ? 'disabled' : 'page-item'">
+                                <button class="page-link" @click="getApartments(currentPage - 1)">Prev</button>
+                            </li>
+                            <li :class="currentPage == i ? 'disabled' : 'page-item'" v-for="i in lastPage" :key="i">
+                                <button class="page-link" @click="getApartments(i)">{{ i }}</button>
+                            </li>
+                            <li :class="currentPage == lastPage ? 'disabled' : 'page-item'">
+                                <button class="page-link" @click="getApartments(currentPage + 1)">Next</button>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
@@ -88,10 +87,6 @@
 
 <style lang="scss" scoped>
 
-    .bg-pink{
-        background: rgb(237,130,172);
-        background: linear-gradient(0deg, rgba(237,130,172,1) 15%, rgba(255,255,255,1) 50%);
-    }
     .card-img-top{
         img{
             max-width: 300px;
