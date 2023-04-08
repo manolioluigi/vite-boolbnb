b61631bd6e395f40ec94d872ae69df8e13fb1643
manolioluigi <luigi.manolio1999@gmail.com>
2023-04-08 12:09:03 +0200

    fix

--- src/components/ApartmentCard.vue
+++ src/components/ApartmentCard.vue
@@ -18,15 +18,16 @@ export default{
     <router-link :to="{ name: 'single_apartment', params: { slug: apartment.slug }}" class="link-apartment" target="_blank">
         <div class="cards" id="cards-search">
             <img class="cards-img" :src="apartment.cover_img != null ? `${store.baseUrl}/storage/${apartment.cover_img}` : 'https://picsum.photos/400/300'" :alt="apartment.title">
+            <span v-if="apartment.sponsorship_id"><i class="fas fa-medal spons-badge"></i></span>
             <div class="cards-list p-4">
                 <ul class="list-unstyled d-flex flex-column">
-                    <li class="me-4 pb-2"><i class="me-1 fas fa-door-closed"></i><span>{{apartment.room_n}} Local</span></li>
-                    <li class="me-4 pb-2"><i class="me-1 fas fa-bed"></i><span>{{apartment.bed_n}} Bed</span></li>
-                    <li class="me-4 pb-2"><i class="me-1 fas fa-shower"></i><span>{{apartment.bath_n}} Bath</span></li>
-                    <li class="me-4 pb-2"><i class="me-1 fas fa-house-chimney"></i><span>{{apartment.square_meters}} Square Meters</span></li>
+                        <li class="me-4 pb-2"><i class="me-1 fas fa-door-closed"></i><span>{{apartment.room_n}} Local</span></li>
+                        <li class="me-4 pb-2"><i class="me-1 fas fa-bed"></i><span>{{apartment.bed_n}} Bed</span></li>
+                        <li class="me-4 pb-2"><i class="me-1 fas fa-shower"></i><span>{{apartment.bath_n}} Bath</span></li>
+                        <li class="me-4 pb-2"><i class="me-1 fas fa-house-chimney"></i><span>{{apartment.square_meters}} Square Meters</span></li>
                 </ul> 
             </div>
-            <div class="title d-flex my-3 ellipsis ellipsis-cont">
+            <div class="title d-flex my-3 ellipsis ellipsis-cont d-flex justify-content-between">
                 <h3 class="flex-wrap d-flex">{{ apartment.title }}</h3>
             </div>
         </div>
@@ -92,6 +93,16 @@ export default{
         color: black;
         text-decoration: none;
     }
+}
+
+.spons-badge{
+ font-size: 50px;
+ color: gold;
+
+ border-radius: 50%;
+ position: absolute;
+ top: 0;
+ right: 10px;
 }
 
 </style>
